7fd3831d4fb88c00b189f7cfdc27b12413ca4882
LGoliatt <goliatt@gmail.com>
2022-10-16 19:34:20 -0300

    goliatt dom 16 out 2022 19:34:19 -03

--- tpot_best_model.py
+++ tpot_best_model.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectFwe, f_regression
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.svm import LinearSVR
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,12 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: -0.13527672954870223
-exported_pipeline = make_pipeline(
-    SelectFwe(score_func=f_regression, alpha=0.047),
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    LinearSVR(C=0.1, dual=True, epsilon=0.1, loss="epsilon_insensitive", tol=1e-05)
-)
+# Average CV score on the training set was: -0.14247634360470773
+exported_pipeline = RidgeCV()
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
